019f54eb0ce6e172928c5a7e4a5e0550347b6220
Glen <jiayuan.xu@mail.mcgill.ca>
2020-11-17 03:26:29 -0500

    go back to ben's commit

--- berry-extension/popup.js
+++ berry-extension/popup.js
@@ -1,14 +1,3 @@
-var tempVote
-
-var options;
-var votes;
-
-function initialData() {
-	for (var i = 0; i < 5; i++) {
-
-	}
-}
-
 var options = ["Yes","No","Maybe"];
 var responses = [30, 41, 23];
 
